88daaec62a28393077ea2de8119f12b90e1ec086
bryan <bryan@macbookpro.311cub.net>
2017-12-14 15:35:25 -0700

    match docker cgroup driver

--- attributes/services.rb
+++ attributes/services.rb
@@ -1,6 +1,6 @@
 
 default['kubernetes']['args'] = {
- :kubelet => '--logtostderr=true -v=2 --allow-privileged=true --api-servers=http://127.0.0.1 --kubeconfig=/var/lib/kubelet/kubeconfigi --cgroup-driver=systemd',
+ :kubelet => '--logtostderr=true -v=2 --allow-privileged=true --api-servers=http://127.0.0.1 --kubeconfig=/var/lib/kubelet/kubeconfigi --cgroup-driver=cgroupfs',
 
  :proxy   => '--logtostderr=true -v=2 --master=http://127.0.0.1:8080 --kubeconfig=/var/lib/kube-proxy/kubeconfig'
 }

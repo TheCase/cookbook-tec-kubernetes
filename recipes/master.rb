0ac7a9116bc3ade17022a64788642494bfc65053
bryan <bryan@macbookpro.311cub.net>
2017-12-14 15:50:28 -0700

    test changes

--- recipes/master.rb
+++ recipes/master.rb
@@ -1,16 +1,14 @@
 include_recipe 'tec-kubernetes::docker'
 include_recipe 'tec-kubernetes::install'
 
-
-
 # master services
-%w(apiserver controller-manager scheduler).each do |container|
-  execute "load/run kube-#{container}" do
-    cwd File.join(node['kubernetes']['root'], 'kubernetes/server/bin')
-    command "docker load -i ./kube-#{container}.tar && docker run -d gcr.io/google_containers/kube-#{container}:$(cat kube-#{container}.docker_tag)"
-    not_if ("docker images | grep #{container}")
-  end
-end
+#%w(controller-manager scheduler).each do |container|
+#  execute "load/run kube-#{container}" do
+#    cwd File.join(node['kubernetes']['root'], 'kubernetes/server/bin')
+#    command "docker load -i ./kube-#{container}.tar && docker run -d gcr.io/google_containers/kube-#{container}:$(cat kube-#{container}.docker_tag)"
+#    not_if ("docker images | grep #{container}")
+#  end
+#end
 
 # etcd
 package 'etcd'
@@ -18,8 +16,6 @@ package 'etcd'
 service 'etcd' do 
   action [ :enable, :start ]
 end
-
-include_recipe 'tec-kubernetes::services'
 
 # api service
 directory '/var/lib/kube-apiservice'
@@ -36,3 +32,5 @@ service 'kube-apiservice' do
   provider Chef::Provider::Service::Systemd
   action [ :enable, :start ]
 end
+
+include_recipe 'tec-kubernetes::services'

a7b320caa318ea063c443e27d866008f570c1faa
bryan <bryan@macbookpro.311cub.net>
2017-12-14 15:52:00 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@repulsor.net'
 license          'All rights reserved'
 description      'Installs/Configures kubernetes and other related bits'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 depends 'docker'
 depends 'swap_device'
